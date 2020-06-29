574c01ba97f00af46f1604075f149428fa75da3c
dimitarg <dimitar.georgiev.bg@gmail.com>
2020-06-29 20:27:41 +0300

    Fmt

--- src/test/scala/com/dimitarg/example/ExampleResSuite.scala
+++ src/test/scala/com/dimitarg/example/ExampleResSuite.scala
@@ -8,17 +8,19 @@ object ExampleResSuite extends PureResourceSuite {
   override type R = SharedResource
 
   override def sharedResource: Resource[IO, SharedResource] = for {
-   _ <- Resource.liftF(IO.pure(println("acquiring shared resource")))
+    _ <- Resource.liftF(IO.pure(println("acquiring shared resource")))
     res <- Resource.liftF(IO.pure(
       SharedResource(FooResource(), BarResource(42))
     ))
   } yield res
 
-  override def suitesStream: fs2.Stream[IO, RTest[SharedResource]] = Stream(
-    test[SharedResource]("some test")(res => {
-      expect(res.bar.value == 42)
-    })) ++
-    Stream.emits(FooSuite.tests)
-      .local[SharedResource](_.foo) ++
-    BarSuite.tests.local(_.bar)
+  override def suitesStream: fs2.Stream[IO, RTest[SharedResource]] =
+      Stream(
+        test[SharedResource]("some test")(res => {
+        expect(res.bar.value == 42)
+      })) ++
+      Stream.emits(FooSuite.tests)
+        .local[SharedResource](_.foo) ++
+      BarSuite.tests
+        .local(_.bar)
 }
