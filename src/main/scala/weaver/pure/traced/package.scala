80854487eb70747ce0914705c725b53bb2743679
Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
2022-11-03 23:39:17 +0200

    Trace weaver expectation failures

--- src/main/scala/weaver/pure/traced/package.scala
+++ src/main/scala/weaver/pure/traced/package.scala
@@ -5,6 +5,8 @@ import cats.effect.IO
 import fs2.Stream
 import natchez.Span
 import weaver.{SourceLocation, Expectations}
+import cats.data.NonEmptyList
+import weaver.AssertionException
 
 package object traced {
 
@@ -12,8 +14,24 @@ package object traced {
 
   def tracedTest(name: String)(run: Span[IO] => IO[Expectations])(implicit loc: SourceLocation): TracedTest =
     ReaderT { parent =>
-      parent.span(name).use(span => weaver.pure.test(name)(run(span)))
+      parent.span(name).use{ span =>
+        weaver.pure.test(name)(run(span))
+          .flatTap { test =>
+            traceExpectationFailures(span, test.run())
+          }
+      }
     }
+
+  private def traceExpectationFailures(span: Span[IO], x: Expectations): IO[Unit] =
+    x.run.fold(
+      xs => span.put("test.expectation.error" -> assertionExceptionsToString(xs)),
+      _ => IO.unit
+    )
+
+  private def assertionExceptionsToString(xs: NonEmptyList[AssertionException]): String =
+    xs.toList.map { x =>
+      (x.message :: x.locations.toList.map(_.toString())).mkString("\n")
+    }.mkString("\n")
 
   def tracedParSuite(name: String)(suite: List[TracedTest])(implicit rootSpan: Span[IO]): Stream[IO,Test] =
     tracedSuite(weaver.pure.parSuite)(name)(suite)(rootSpan)
