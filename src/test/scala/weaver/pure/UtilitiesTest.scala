eda90498b797e88a41f99b0995858fa15b02e4f7
Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
2025-06-04 11:36:54 +0300

    Updates/2025 06 04 (#265)
    * Use flakes
    
    * Update sbt
    
    * Update scala-library to 2.13.16 (#252)
    
    * Update scala-library to 2.13.16
    
    * Regenerate GitHub Actions workflow
    
    Executed command: sbt githubWorkflowGenerate
    
    * Update scala3-library to 3.3.6 (#263)
    
    * Update scala3-library to 3.3.6
    
    * Regenerate GitHub Actions workflow
    
    Executed command: sbt githubWorkflowGenerate
    
    ---------
    
    Co-authored-by: Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
    
    * Update fs2-io to 3.12.0 (#261)
    
    Co-authored-by: Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
    
    * Update natchez-core, natchez-honeycomb, ... to 0.3.8 (#262)
    
    Co-authored-by: Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
    
    * Update sbt-github-actions to 0.25.0 (#259)
    
    * Update scalafmt-core to 3.8.6 (#255)
    
    * Update scalafmt-core to 3.8.6
    
    * Reformat with scalafmt 3.8.6
    
    Executed command: scalafmt --non-interactive
    
    * Add 'Reformat with scalafmt 3.8.6' to .git-blame-ignore-revs
    
    ---------
    
    Co-authored-by: Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
    
    * Update sbt-scalafmt to 2.5.4 (#253)
    
    * Update weaver-cats, weaver-scalacheck to 0.9.0 (#264)
    
    Co-authored-by: Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
    
    * Update sbt-pgp to 2.3.1 (#248)
    
    Co-authored-by: Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
    
    * Fix test due to expectly -> clue
    
    * Drop scala 2.12 support
    
    ---------
    
    Co-authored-by: Scala Steward <43047562+scala-steward@users.noreply.github.com>

--- src/test/scala/weaver/pure/UtilitiesTest.scala
+++ src/test/scala/weaver/pure/UtilitiesTest.scala
@@ -11,10 +11,11 @@ object UtilitiesTest extends Suite {
   override def suitesStream: Stream[IO, Test] = Stream(
     pureTest("outcomeToString contains source location and assertion failure body") {
 
-      val test = Test("some test", expect(1 === 2))
+      val test = Test("some test", expect(clue(1 === 2)))
       val testOutcome = toTestOutcome(test)
       val asString = outcomeToString(testOutcome)
 
+      println(asString)
       expect(asString.contains("UtilitiesTest.scala")) and
         expect(asString.contains("1 === 2"))
 
